f43d43116370b2a95c2b02c235b0bd75b528e32f
Mohamad <alkerdi.mhmd@gmail.com>
2025-09-13 11:30:04 +0300

    feat: Update user roles and disable account registration UI

--- src/models/User.js
+++ src/models/User.js
@@ -65,7 +65,7 @@ const userSchema = new mongoose.Schema(
 		},
 		role: {
 			type: String,
-			enum: ["rider", "user", "manager", "admin"],
+			enum: ["customer", "rider", "staff", "user", "manager", "admin"],
 			default: "user",
 		},
 		isActive: {
