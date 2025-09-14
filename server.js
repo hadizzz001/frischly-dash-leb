a58d2037c42d8cbec53311ad5670548bc89c0927
Mohamad <alkerdi.mhmd@gmail.com>
2025-09-14 22:52:15 +0300

    fix: Adjust rate limiting configuration for production environment

--- server.js
+++ server.js
@@ -69,8 +69,8 @@ app.use(
 // Rate limiting - Disabled in development
 if (process.env.NODE_ENV === "production") {
 	const limiter = rateLimit({
-		windowMs: 15 * 60 * 1000, // 15 minutes
-		max: 200, // limit each IP to 100 requests per windowMs
+		windowMs: 10 * 60 * 1000, // 15 minutes
+		max: 2000, // limit each IP to 100 requests per windowMs
 		message: {
 			success: false,
 			message: "Too many requests from this IP, please try again later.",
